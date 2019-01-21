3f68dad6e4857bfc68351e140faa978a9085ffed
fabien vantorre <fabien.vantorre@hotmail.fr>
2019-01-21 10:15:12 +0100

    jacksonDatabindTest

---
+++ src/main/java/com/miage/altea/bo/PokemonType.java
@@ -0,0 +1,15 @@
+package com.miage.altea.bo;
+
+public class PokemonType {
+
+    private int id;
+    private int baseExperience;
+    private int height;
+    private String name;
+    private Sprites sprites;
+    private Stats stats;
+    private int weight;
+
+
+
+}
---
+++ src/main/java/com/miage/altea/bo/Sprites.java
@@ -0,0 +1,8 @@
+package com.miage.altea.bo;
+
+public class Sprites {
+
+    private String back_default;
+    private String front_default;
+
+}
